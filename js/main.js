ce2dda58903fcf28a5ebd2ca2885b01a3b235e44
dobrovolskyv <malak3@mail.ru>
2021-10-25 22:10:34 +0300

    practic

---
+++ js/main.js
@@ -0,0 +1,38 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".tabheader__item");
+  const tabsContent = document.querySelectorAll(".tabcontent");
+  const tabsParent = document.querySelector(".tabheader");
+
+  function hideTabs() {
+    tabsContent.forEach((item) => {
+      item.classList.add("hide");
+      item.classList.remove("show", "fade");
+    });
+
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
+    });
+  }
+
+  function showTabs(i = 0) {
+    tabsContent[i].classList.add("show", "fade");
+    tabsContent[i].classList.remove("hide");
+    tabs[i].classList.add("tabheader__item_active");
+  }
+
+  hideTabs();
+  showTabs();
+
+  tabsParent.addEventListener("click", (event) => {
+    const target = event.target;
+
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabs();
+          showTabs(i);
+        }
+      });
+    }
+  });
+});
