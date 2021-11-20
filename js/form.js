7a2c06bee2b6775fc55278e5add09647701b7f6a
dobrovolskyv <malak3@mail.ru>
2021-11-20 14:36:18 +0300

    fetch and ajax

--- js/form.js
+++ js/form.js
@@ -1,50 +1,50 @@
-const forms = document.querySelectorAll("form");
-const message = {
-  loading: "in progress",
-  success: "done",
-  failure: "fail...",
-};
-forms.forEach((item) => postData(item));
+// const forms = document.querySelectorAll("form");
+// const message = {
+//   loading: "in progress",
+//   success: "done",
+//   failure: "fail...",
+// };
+// forms.forEach((item) => postData(item));
 
-function postData(form) {
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+// function postData(form) {
+//   form.addEventListener("submit", (e) => {
+//     e.preventDefault();
 
-    const statusMessage = document.createElement("div");
-    statusMessage.classList.add("status");
-    statusMessage.textContent = message.loading;
-    form.append(statusMessage);
+//     const statusMessage = document.createElement("div");
+//     statusMessage.classList.add("status");
+//     statusMessage.textContent = message.loading;
+//     form.append(statusMessage);
 
-    const request = new XMLHttpRequest();
-    request.open("POST", "/server.php");
+//     const request = new XMLHttpRequest();
+//     request.open("POST", "/server.php");
 
-    request.setRequestHeader("Content-type", "application/json");
-    const formData = new FormData(form);
+//     request.setRequestHeader("Content-type", "application/json");
+//     const formData = new FormData(form);
 
-    const object = {};
-    formData.forEach(function (value, key) {
-      object[key] = value;
-    });
+//     const object = {};
+//     formData.forEach(function (value, key) {
+//       object[key] = value;
+//     });
 
-    const json = JSON.stringify(object);
+//     const json = JSON.stringify(object);
 
-    request.send(json);
+//     request.send(json);
 
-    request.addEventListener("load", () => {
-      if (request.status === 200) {
-        console.log(request.response);
-        statusMessage.textContent = message.success;
-        form.reset();
-        setTimeout(() => {
-          statusMessage.remove();
-        }, 2000);
-      } else {
-        statusMessage.textContent = message.failure;
-        form.reset();
-        setTimeout(() => {
-          statusMessage.remove();
-        }, 2000);
-      }
-    });
-  });
-}
+//     request.addEventListener("load", () => {
+//       if (request.status === 200) {
+//         console.log(request.response);
+//         statusMessage.textContent = message.success;
+//         form.reset();
+//         setTimeout(() => {
+//           statusMessage.remove();
+//         }, 2000);
+//       } else {
+//         statusMessage.textContent = message.failure;
+//         form.reset();
+//         setTimeout(() => {
+//           statusMessage.remove();
+//         }, 2000);
+//       }
+//     });
+//   });
+// }
--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,5 @@
 const modalTrigger = document.querySelectorAll("[data-modal]");
 const modal = document.querySelector(".modal");
-const modalCloseBtn = document.querySelector("[data-close");
 
 function openModal() {
   modal.classList.add("show");
@@ -18,10 +17,9 @@ function closeModal() {
   modal.classList.remove("show");
   document.body.style.overflow = "";
 }
-modalCloseBtn.addEventListener("click", closeModal);
 
 modal.addEventListener("click", (e) => {
-  if (e.target === modal) {
+  if (e.target === modal || e.target.getAttribute("data-close") == "") {
     closeModal();
   }
 });
@@ -32,7 +30,7 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
-const modalTimeout = setTimeout(openModal, 2000);
+const modalTimeout = setTimeout(openModal, 50000);
 
 function showModalByScroll() {
   if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -41,3 +39,115 @@ function showModalByScroll() {
   }
 }
 window.addEventListener("scroll", showModalByScroll);
+
+//form AJAX
+
+const forms = document.querySelectorAll("form");
+const message = {
+  loading: "../img/form/054 spinner.svg",
+  success: "done",
+  failure: "fail...",
+};
+forms.forEach((item) => postData(item));
+
+function postData(form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const statusMessage = document.createElement("img");
+    statusMessage.src = message.loading;
+    statusMessage.style.cssText = `
+    display: block;
+    margin: 0 auto`;
+    form.append(statusMessage);
+    form.insertAdjacentElement("afterend", statusMessage);
+
+    const request = new XMLHttpRequest();
+    request.open("POST", "/server.php");
+
+    request.setRequestHeader("Content-type", "application/json");
+    const formData = new FormData(form);
+
+    const object = {};
+    formData.forEach(function (value, key) {
+      object[key] = value;
+    });
+
+    const json = JSON.stringify(object);
+
+    request.send(json);
+
+    request.addEventListener("load", () => {
+      if (request.status === 200) {
+        console.log(request.response);
+        showThanksModal(message.success);
+        form.reset();
+        statusMessage.remove();
+      } else {
+        showThanksModal(message.failure);
+      }
+    });
+  });
+}
+
+// form fetch
+// function postData(form) {
+//   form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+
+//     const statusMessage = document.createElement("img");
+//     statusMessage.src = message.loading;
+//     statusMessage.style.cssText = `
+//     display: block;
+//     margin: 0 auto`;
+//     form.append(statusMessage);
+//     form.insertAdjacentElement("afterend", statusMessage);
+
+//     const object = {};
+//     formData.forEach(function (value, key) {
+//       object[key] = value;
+//     });
+
+//     fetch("server.php", {
+//       method: "POST",
+//       headers: { "Content-type": "application/json" },
+//       body: JSON.stringify(object), //formData
+//     })
+//       .then((data) => data.text())
+//       .then((data) => {
+//         console.log(data);
+//         showThanksModal(message.success);
+
+//         statusMessage.remove();
+//       })
+//       .catch(() => {
+//         showThanksModal(message.failure);
+//       })
+//       .finform.reset();
+//     ally(() => {});
+//   });
+// }
+
+//form modal bueaty modal
+function showThanksModal(message) {
+  const prevModalDialog = document.querySelector(".modal__dialog");
+
+  prevModalDialog.classList.add("hide");
+  openModal();
+
+  const thanksModal = document.createElement("div");
+  thanksModal.classList.add("modal__dialog");
+  thanksModal.innerHTML = `
+  <div class="modal__content">
+    <div class="modal__close" data-close>×</div>
+    <div class="modal__title">${message}</div>
+  </div>`;
+
+  document.querySelector(".modal").append(thanksModal);
+  setTimeout(() => {
+    thanksModal.remove();
+    prevModalDialog.classList.add("show");
+    prevModalDialog.classList.remove("hide ");
+    closeModal();
+  }, 4000);
+}
